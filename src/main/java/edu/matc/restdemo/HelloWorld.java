49c5bcc013e403ca19fb5539ef96ae01e878c0b5
Gunther Knutson <guntherknutson@gmail.com>
2016-10-11 12:22:19 -0700

    Initial commit

---
+++ src/main/java/edu/matc/restdemo/HelloWorld.java
@@ -0,0 +1,29 @@
+package edu.matc.restdemo;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+
+@Path("/hello")
+public class HelloWorld {
+    // The Java method will process HTTP GET requests
+    @GET
+
+    @Path("?{param}")
+    // The Java method will produce content identified by the MIME Media type "text/plain"
+    @Produces("text/plain")
+    public Response getMessage(@PathParam("param") String name) {
+        // Return a simple message
+        String output;
+        if(name == null) {
+            output = "Hello World";
+        }else{
+            output = "Hello" + name + "!";
+        }
+        return Response.status(200).entity(output).build();
+    }
+
+}
